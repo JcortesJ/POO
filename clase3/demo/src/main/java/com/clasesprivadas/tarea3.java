e7fd799e52db84a99f57ac5d5287d4431e3f0808
juan manuel cortes <87251455+JcortesJ@users.noreply.github.com>
2021-11-03 16:26:59 -0500

    Añadidos get y set para Papitos, profesores (...)
    Tambien añadi un get y set en Estudiante y grupos

--- clase3/demo/src/main/java/com/clasesprivadas/tarea3.java
+++ clase3/demo/src/main/java/com/clasesprivadas/tarea3.java
@@ -1,58 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
-
-
-import java.util.List;
-
-/**
- *
- * @author juan manuel
- */
-public class tarea3 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
-    }
-}
-
-/*
-Una institución educativa de carácter público se encuentra desarrollando un sistema 
-de información académica similar al SIA. Para tal fin, busca apoyo de las y los estudiantes
-que tienen conocimiento de programación para que aporten sus ideas e implementen un prototipo
-diseñado desde el paradigma de la programación orientada a objetos. Deben tener en cuenta los 
-siguientes requerimientos:
-
-Hacer el modelo del sistema con las siguientes características:
-
-Abstraer como mínimo 7 clases con sus atributos
-
-Realizar el diagrama de clases UML
-
-Contar con objetos como atributos para relacionar las clase (asociaciones)
-
-Encapsular cada una de las clases
-
-Hacer los get y set de cada uno de los atributos en cada una de las clases
-
-Entregar, el diagrama UML y las clases del proyecto.
-
-
-*/
-
 class Profesor{
     private String nombre;
     private int edad;
     private String ID;
     private String email;
-    private List<Grupos> dictados;
-    //private List<Subsidios> obtiene;
-    //private List <Notas> califica;
-    //pendiente para más tarde
+    private ArrayList<Grupos> dictados;
+    private ArrayList<Notas> califica;
+    //METODOS GET Y SET
+    
+    public void setNombre (String a){
+        this.nombre = a;
+    }
+    
+    public String getNombre ( ){
+        return this.nombre;
+    }
+    
+        public void setEdad (int i){
+            this.edad = i;
+    }
+    
+    public int getEdad ( ){
+        return this.edad;
+    }
+    
+        public void setID (String ID ){
+             this.ID = ID;
+    }
+    
+    public String getID ( ){
+        return this.ID;
+    }
+    
+        public void setEmail (String a ){
+           this.email=a;
+    }
+    
+    public String getEmail ( ){
+        return this.email;
+    }
+    
+    //GET Y SET PARA LOS ARRAYLIST
+    //PARA LOS get ES indispensable tener el indice de cada elemento
+    //recuerda que a los set se les da de argumento un objeto.
+        public void setGrupo (Grupos e){
+              this.dictados.add(e);
+    }
+    
+    public Grupos getGrupo (int a){
+        return dictados.get(a);
+    }
+    
+       public void setNotas (Notas e){
+              this.califica.add(e);
+    }
+    
+    public Notas getNotas (int a){
+        return califica.get(a);
+    }
+
+
+    
+    
 }
 
 class Estudiante{
@@ -60,16 +68,51 @@ class Estudiante{
     private int edad;
     private String usuario;
     private String ID;
-    private List<Grupos> atiende;
-    private List<Subsidios> obtiene;
-    private List<Materias> materiasinscritas;
-    //private List <Notas> recibe;
+    private Papitos acudiente;
+    private ArrayList<Grupos> atiende;
+    private ArrayList<Subsidios> obtiene;
+    private ArrayList<Materias> materiasinscritas;
+    private ArrayList<Notas> recibe;
+    
+    public void setAcudiente(Papitos a){
+        this.acudiente = a;
+    }
+    
+    public Papitos getAcudiente(){
+        return this.acudiente;
+    }
+    
 }
 
 class Papitos{
     private String nombre;
-    private int ID;
-    private List<Estudiante> acudiente;
+    private String ID;
+    private ArrayList<Estudiante> acudiente;
+    
+    public void setNombre( String a){
+        this.nombre = a;
+    }
+    
+    public String getNombre( String a){
+        return this.nombre;
+    }
+    
+       public void setID( String a){
+        this.ID = a;
+    }
+    
+    public String getID( String a){
+        return this.ID;
+    }
+    
+    public void setAcudiente(Estudiante a){
+        acudiente.add(a);
+    }
+    
+    public Estudiante getAcudiente(int a){
+        return acudiente.get(a);
+    }
+    
     
 }
 
@@ -78,29 +121,51 @@ class Papitos{
 class Grupos{
     private int numero;
     private String[] horario;
-    private String[] vecesAlaSemana;
-    private List<Materias> nombre;
-    private List<Profesor> dadoPor;
+    private ArrayList<Materias> nombre;
+    private Profesor profesor;
+    private int cupos;
+    private ArrayList<Estudiante> estudiantes;
+    //Los estudiantes deben poder inscribirse a los grupos
+    private ArrayList<Notas> notasGrupo;
     
-            }
+    public void setHorario(String i, int indice){ //habria que decirle al usuario, que cada dia de la semana tiene un numero
+        //y que segun ese dia pues ponga el respectivo.
+        this.horario[indice] = i; 
+    }
+    
+    public void getHorario(){
+        for(int i=0; i< horario.length; i++){
+            System.out.println(horario[i]);
+        }
+        //queda pendiente hacer un override del metodo String, para mostrar de una forma más
+        //apropiada la información. 
+    }
+    
+    }
+    
+            
 
 class Subsidios{
     private String tipo;
     private double valor;
     private int duracionSemestres;
-    private List<Estudiante> beneficiarios;
+    private ArrayList<Estudiante> beneficiarios;
 }
 
 class Notas{
     private int valor;
-    private List<Estudiante> asignar;
+    private ArrayList<Estudiante> asignar;
+    
 }
 
 class Materias{
    private int codigo;
    private String nombre;
    private int creditos;
-   private List<Grupos> grupos;
+   private ArrayList<Grupos> grupos;
 }
+    
+
+
     
 
